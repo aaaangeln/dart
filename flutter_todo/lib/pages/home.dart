971fc5a0ffea2e42f556f7964df7802ede62f4ad
aaaangeln <aaaangeln01@gmail.com>
2023-07-31 20:51:13 -0400

    last

--- flutter_todo/lib/pages/home.dart
+++ flutter_todo/lib/pages/home.dart
@@ -9,52 +9,84 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-
+  String user_todo='';
+  List todoList = [];
 
   @override
   void initState() {
     super.initState();
-    todoList.addAll(['buy','set']);
+
+    todoList.addAll(['buy','learn','milk']);
   }
-  List todoList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[500],
+      backgroundColor: Colors.deepPurple[100],
       appBar: AppBar(
-        title: const Text('Список дел'),
+        backgroundColor: Colors.deepPurple,
+        title: Text('СПИСОК ДЕЛ', ),
         centerTitle: true,
       ),
       body: ListView.builder(
         itemCount: todoList.length,
-        itemBuilder: (context, index){
+        itemBuilder: (BuildContext context, int index){
           return Dismissible(
               key: Key(todoList[index]),
               child: Card(
                 child: ListTile(
                   title: Text(todoList[index]),
                   trailing: IconButton(
-                    icon: const Icon(
-                      Icons.delete,
-                      color: Colors.red,
-                    ),
-                    onPressed: () {
+                    icon: Icon(Icons.delete, color: Colors.deepPurple),
+                    onPressed: (){
                       setState(() {
-                        todoList.removeAt(todoList[index]);
+                        todoList.removeAt(index);
                       });
                     },
                   ),
                 ),
               ),
             onDismissed: (direction){
-                  setState(() {
-                    todoList.remove(todoList[index]);
-                  });
+                setState(() {
+                  todoList.removeAt(index);
+                });
             },
+
           );
-        }
-      )
+        },
+
+      ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.deepPurple,
+          onPressed: (){
+            showDialog(context: context, builder: (BuildContext context){
+              return AlertDialog(
+                title: Text('Введите новое дело: '),
+                content: TextField(
+                  onChanged: (String value){
+                    user_todo=value;
+                  },
+                ),
+                actions: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.deepPurple,
+                    ),
+                      onPressed: (){
+                        setState(() {
+                          todoList.add(user_todo);
+                        });
+                        Navigator.of(context).pop();
+                  },
+                      child: Text('Добавить'))
+                ],
+              );
+            }
+            );
+          },
+            child:
+              Icon(Icons.add, color: Colors.white,),
+        ),
     );
   }
 }
